35d471578676e6311704f466d16d161c06ee9f02
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-05-18 08:22:41 -0700

    added some headers

--- s_shell.h
+++ s_shell.h
@@ -2,4 +2,12 @@
 #define S_SHELL_H
 
 /* lists of all prototype variable names */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include < sys/wait.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
 
